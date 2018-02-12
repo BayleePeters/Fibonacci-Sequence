cefebadc66352ef36561a777c2db6f7273836c93
Baylee Peters <bpeters4@friars.providence.edu>
2018-02-11 20:31:47 -0500

    project12.py

---
+++ project12.py
@@ -0,0 +1,22 @@
+# Baylee Peters Project 12: Fibonacci Sequence
+
+num1=0 #first number
+num2=1 #second number
+
+sum=0 #place holder
+count=2 #while loop counter
+
+print (str(1) + ":" + str(1))
+while (count<26):
+	sum=num1 + num2
+	print(str(count) + ":" + str(sum))
+	num1=num2
+	num2=sum
+	count= count + 1
+	
+	
+	
+
+	
+
+
